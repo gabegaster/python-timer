0e3dab30e7ed0f787df456f9cf0c29e8dfd0f2a2
gabe gaster <gabe@gabegaster.com>
2015-10-12 15:38:21 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+import timer
+
+github_url = 'https://github.com/gabegaster/python-timer'
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.readlines()
+
+# read in the description from README
+long_description = "\n".join(read("README.md"))
+
+install_requires = []
+
+setup(
+    name="django-offlinecdn",
+    version=offlinecdn.VERSION,
+    description=("A wrapper for long-running tasks that iterate over many things, "
+                 "printing friendly 'percentage done' messages to standard error."),
+    long_description=long_description,
+    url=github_url,
+    download_url="%s/archives/master" % github_url,
+    author='Gabe Gaster',
+    author_email='gabe@datascopeanalytics.com',
+    license='MIT',
+    packages=[
+        'python-timer',
+    ],
+    install_requires=install_requires,
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
